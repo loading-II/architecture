c16d9ae88f0159c6ed29c3dab04074a0402cccfb
Guoql <loading_vip@126.com>
2022-09-14 16:06:37 +0800

    β
    with arouter

--- architecture/src/main/java/com/frame/arc/ui/activity/BaseActivity.kt
+++ architecture/src/main/java/com/frame/arc/ui/activity/BaseActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.alibaba.android.arouter.launcher.ARouter
 import com.frame.arc.BaseApp
 import kotlinx.coroutines.*
 
@@ -22,6 +23,7 @@ open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        ARouter.getInstance().inject(this)
     }
 
     protected fun doLaunch(block: suspend CoroutineScope.() -> Unit): Job {
